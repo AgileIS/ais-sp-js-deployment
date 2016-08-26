3f40a8ac697b9f1cf2abd5e9c97a2175f7db5934
Daniel Zeller <dzeller@agile-is.de>
2016-08-26 10:23:52 +0200

    exported initAuth

--- src/deploy.ts
+++ src/deploy.ts
@@ -6,6 +6,7 @@ import {ISPObjectHandler} from "./interface/ObjectHandler/ispobjecthandler";
 import {SiteHandler} from "./ObjectHandler/SiteHandler";
 import {ListHandler} from "./ObjectHandler/ListHandler";
 import {FieldHandler} from "./ObjectHandler/FieldHandler";
+import {initAuth} from "./lib/initClient";
 import * as fetch from "node-fetch";
 
 class MyConsoleLogger implements LogListener {
@@ -84,39 +85,4 @@ export function chooseAndUseHandler(config: any, siteUrl: string) {
             }
         }
     });
-}
-
-
-
-export interface Global {
-    Headers: any;
-    Request: any;
-    fetch: any;
-}
-
-export function initAuth(url: string, username: string, password: string) {
-    // Fixed missing Header & Request in node
-    let fetch = require('node-fetch');
-    global["Headers"] = fetch.Headers;
-    global["Request"] = fetch.Request;
-    global["fetch"] = fetch;
-
-    // var httpNTLMClient = require("./lib/NTLMHttpClient");
-    // httpNTLMClient.options.username = username;
-    // httpNTLMClient.options.password = password;
-    // httpNTLMClient.options.domain = domain;
-    // httpNTLMClient.options.workstation = "";
-
-    // var httpClient = require("agileis-sp-pnp-js/lib/net/HttpClient");
-    // httpClient.HttpClient = httpNTLMClient.client;
-
-    let httpBasicClient = require("./lib/BasicHttpClient");
-
-    let userAndDommain = username.split("\\");
-
-    httpBasicClient.options.username = userAndDommain[0] + "\\" + userAndDommain[1];
-    httpBasicClient.options.password = password;
-
-    let httpClient = require("sp-pnp-js/lib/net/HttpClient");
-    httpClient.HttpClient = httpBasicClient.client;
 }
