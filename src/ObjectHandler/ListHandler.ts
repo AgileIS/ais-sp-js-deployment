39afa100dab8767398e0539aa78a5ec371c1cf78
Daniel Zeller <dzeller@agile-is.de>
2016-09-08 11:48:47 +0200

    Updated ListHandler

--- src/ObjectHandler/ListHandler.ts
+++ src/ObjectHandler/ListHandler.ts
@@ -1,122 +1,141 @@
 import {ISPObjectHandler} from "../interface/ObjectHandler/ispobjecthandler";
 import {Logger} from "sp-pnp-js/lib/utils/logging";
 import {IList, } from "../interface/Types/IList";
-import {ListTemplates} from "../lib/ListTemplates";
+import {ListTemplates} from "../Constants/ListTemplates";
 import * as Types from "sp-pnp-js/lib/sharepoint/rest/types";
 import {Web} from "sp-pnp-js/lib/sharepoint/rest/webs";
 import {List, Lists} from "sp-pnp-js/lib/sharepoint/rest/lists";
 import {Queryable} from "sp-pnp-js/lib/sharepoint/rest/queryable";
-import {RejectAndLog} from "../lib/Util/Util";
+import {RejectAndLog} from "../Util/Util";
 
 export class ListHandler implements ISPObjectHandler {
 
-    execute(config: IList, parent: Promise<Web>):Promise<List> {
-        return new Promise<List>((resolve, reject) => {
-            parent.then((parentInstance) => {
-                Logger.write("enter List execute", 0);
-                // das hier geht noch nicht INstance eventuell überdenken!
-                let list = parentInstance.lists.getByTitle(config.Title);  
-               list.get().then(result => {
-                    resolve(list);
-                });
-            });
+    execute(config: IList, parent: Promise<Web>): Promise<List> {
+        /* return new Promise<List>((resolve, reject) => {
+             parent.then((parentInstance) => {
+                 Logger.write("enter List execute", 0);
+                 let list = parentInstance.lists.getByTitle(config.Title);
+                 list.get().then(result => {
+                     resolve(list);
+                 });
+             });
+ 
+         });*/
 
-        });
-
-    }
-/*    execute(config: IList, url: string, parentConfig: ISite) {
         switch (config.ControlOption) {
             case "Update":
-                return this.UpdateList(config, url);
+                return this.UpdateList(config, parent);
             case "Delete":
-                return this.DeleteList(config, url);
-
+                return this.DeleteList(config, parent);
             default:
-                return this.AddList(config, url);
+                return this.AddList(config, parent);
         }
-    }*/
 
-    private AddList(config: IList, url: string) {
-        let element = config;
-        let spWeb = new Web(url);
-        return new Promise<IList>((resolve, reject) => {
-            Logger.write("config " + JSON.stringify(config), 0);
-            if (element.TemplateType) {
-                spWeb.lists.filter(`RootFolder/Name eq '${element.InternalName}'`).select("Id").get().then((result) => {
-                    if (result.length === 0) {
-                        let propertyHash = this.CreateProperties(element);
-                        spWeb.lists.add(element.InternalName, element.Description, element.TemplateType, element.EnableContentTypes, propertyHash).then((result) => {
-                            result.list.update({ Title: element.Title }).then((result) => {
-                                resolve(element);
-                                Logger.write(`List ${element.Title} created`, 0);
-                            }, (error) => {
-                                RejectAndLog(error, element.InternalName, reject);
-                            });
+    }
+    /*    execute(config: IList, url: string, parentConfig: ISite) {
+            switch (config.ControlOption) {
+                case "Update":
+                    return this.UpdateList(config, url);
+                case "Delete":
+                    return this.DeleteList(config, url);
+    
+                default:
+                    return this.AddList(config, url);
+            }
+        }*/
+
+    private AddList(config: IList, parent: Promise<Web>): Promise<List> {
+        return new Promise<List>((resolve, reject) => {
+            parent.then(parentInstance => {
+                Logger.write("config " + JSON.stringify(config), 0);
+                let list = parentInstance.lists.getByTitle(config.Title);
+                if (list !== undefined) {
+                    if (config.TemplateType) {
+                        parentInstance.lists.filter(`RootFolder/Name eq '${config.InternalName}'`).select("Id").get().then((result) => {
+                            if (result.length === 0) {
+                                let propertyHash = this.CreateProperties(config);
+                                parentInstance.lists.add(config.InternalName, config.Description, config.TemplateType, config.EnableContentTypes, propertyHash).then((result) => {
+                                    result.list.update({ Title: config.Title }).then((result) => {
+                                        resolve(list);
+                                        Logger.write(`List ${config.Title} created`, 0);
+                                    }, (error) => {
+                                        reject(error);
+                                    });
+                                }, (error) => {
+                                    reject(error);
+                                });
+                            } else {
+                                resolve(list);
+                                Logger.write(`List with Internal Name '${config.InternalName}' already exists`, 0);
+                            }
                         }, (error) => {
-                            RejectAndLog(error, element.InternalName, reject);
+                            reject(error);
                         });
-                    } else {
-                        resolve(element);
-                        Logger.write(`List with Internal Name '${element.InternalName}' already exists`, 0);
                     }
-                }, (error) => {
-                    RejectAndLog(error, element.InternalName, reject);
+                    else {
+                        let error = `List Template Type could not be resolved for List: ${config.InternalName}`;
+                        reject(error);
+                    }
+                }
+
+            })
+
+        });
+    }
+
+    private UpdateList(config: IList, parent: Promise<Web>): Promise<List> {
+        return new Promise<List>((resolve, reject) => {
+            parent.then(parentInstance => {
+                Logger.write("config " + JSON.stringify(config), 0);
+                let list = parentInstance.lists.getByTitle(config.Title);
+                parentInstance.lists.filter(`RootFolder/Name eq '${config.InternalName}'`).select("Id").get().then((result) => {
+                    if (result.length === 1) {
+                        let listId = result[0].Id;
+                        let properties = this.CreateProperties(config);
+                        list.update(properties).then(() => {
+                            resolve(list);
+                            Logger.write(`List with Internal Name '${config.InternalName}' updated`, 1);
+                        }).catch((error) => {
+                            reject(error);
+                        });
+                    }
+                    else {
+                        let error = `List with Internal Name '${config.InternalName}' does not exist`;
+                        reject(error);
+                    }
+                }).catch((error) => {
+                    reject(error);
                 });
-            }
-            else {
-                let error = `List Template Type could not be resolved for List: ${element.InternalName}`;
-                RejectAndLog(error, element.InternalName, reject);
-            }
+            })
+
         });
     }
 
-    private UpdateList(config: IList, url: string) {
-        let element = config;
-        let spWeb = new Web(url);
-        return new Promise<IList>((resolve, reject) => {
-            spWeb.lists.filter(`RootFolder/Name eq '${element.InternalName}'`).select("Id").get().then((result) => {
-                if (result.length === 1) {
-                    let listId = result[0].Id;
-                    let properties = this.CreateProperties(element);
-                    spWeb.lists.getById(listId).update(properties).then(() => {
-                        resolve(element);
-                        Logger.write(`List with Internal Name '${element.InternalName}' updated`, 1);
-                    }).catch((error) => {
-                        RejectAndLog(error, element.InternalName, reject);
-                    });
-                }
-                else {
-                    let error = `List with Internal Name '${element.InternalName}' does not exist`;
-                    RejectAndLog(error, element.InternalName, reject);
-                }
-            }).catch((error) => {
-                RejectAndLog(error, element.InternalName, reject);
-            });
-        });
-    }
+    private DeleteList(config: IList, parent: Promise<Web>): Promise<List> {
+        return new Promise<List>((resolve, reject) => {
+            parent.then(parentInstance => {
+                Logger.write("config " + JSON.stringify(config), 0);
+                let list = parentInstance.lists.getByTitle(config.Title);
+                parentInstance.lists.filter(`RootFolder/Name eq '${config.InternalName}'`).select("Id").get().then((result) => {
+                    if (result.length === 1) {
+                        let listId = result[0].Id;
+                        list.delete().then(() => {
+                            let configForDelete = this.CreateProperties(config);
+                            resolve(configForDelete);
+                            Logger.write(`List with Internal Name '${config.InternalName}' deleted`, 1);
+                        }).catch((error) => {
+                            reject(error);
+                        });
+                    }
+                    else {
+                        let error = `List with Internal Name '${config.InternalName}' does not exist`;
+                        reject(error);
+                    }
+                }).catch((error) => {
+                    reject(error);
+                });
+            })
 
-    private DeleteList(config: IList, url: string) {
-        let element = config;
-        let spWeb = new Web(url);
-        return new Promise<IList>((resolve, reject) => {
-            spWeb.lists.filter(`RootFolder/Name eq '${element.InternalName}'`).select("Id").get().then((result) => {
-                if (result.length === 1) {
-                    let listId = result[0].Id;
-                    spWeb.lists.getById(listId).delete().then(() => {
-                        let configForDelete = this.CreateProperties(config);
-                        resolve(configForDelete);
-                        Logger.write(`List with Internal Name '${element.InternalName}' deleted`, 1);
-                    }).catch((error) => {
-                        RejectAndLog(error, element.InternalName, reject);
-                    });
-                }
-                else {
-                    let error = `List with Internal Name '${element.InternalName}' does not exist`;
-                    RejectAndLog(error, element.InternalName, reject);
-                }
-            }).catch((error) => {
-                RejectAndLog(error, element.InternalName, reject);
-            });
         });
     }
 
