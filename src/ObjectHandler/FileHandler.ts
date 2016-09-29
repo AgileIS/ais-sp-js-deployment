bba31e5de494fa2276c33105077ac275b3bf3ab7
Yücel Tasyol <ytasyol@agile-is.de>
2016-09-29 10:43:22 +0200

    added IPromiseResult to FIleHandler

--- src/ObjectHandler/FileHandler.ts
+++ src/ObjectHandler/FileHandler.ts
@@ -3,31 +3,33 @@ import { Web } from "@agileis/sp-pnp-js/lib/sharepoint/rest/webs";
 import { Folders, Folder } from "@agileis/sp-pnp-js/lib/sharepoint/rest/folders";
 import { Files, File} from "@agileis/sp-pnp-js/lib/sharepoint/rest/files";
 import { List } from "@agileis/sp-pnp-js/lib/sharepoint/rest/lists";
-import { ISPObjectHandler } from "../interface/ObjectHandler/ispobjecthandler";
-import { IFile } from "../interface/Types/IFile";
-import { IFolder } from "../interface/Types/IFolder";
-import { IItem } from "../interface/Types/IItem";
+import { Item } from "@agileis/sp-pnp-js/lib/sharepoint/rest/items";
+import { ISPObjectHandler } from "../interfaces/ObjectHandler/ispobjecthandler";
+import { IFile } from "../interfaces/Types/IFile";
+import { IFolder } from "../interfaces/Types/IFolder";
+import { IItem } from "../interfaces/Types/IItem";
 import { ControlOption } from "../Constants/ControlOption";
-import { Resolve, Reject } from "../Util/Util";
+import { IPromiseResult } from "../interfaces/IPromiseResult";
+import { Util } from "../Util/Util";
 import * as fs from "fs";
 import * as mime from "mime";
 
 export class FileHandler implements ISPObjectHandler {
-    public execute(fileFolderConfig: IFile | IFolder, parentPromise: Promise<Web | Folder | List>): Promise<File | Folder> {
-        return new Promise<File | Folder>((resolve, reject) => {
+    public execute(fileFolderConfig: IFile | IFolder, parentPromise: Promise<IPromiseResult<Web | Folder | List>>): Promise<IPromiseResult<File | Folder>> {
+        return new Promise<IPromiseResult<File | Folder>>((resolve, reject) => {
             parentPromise.then(parentResult => {
-                parentResult.get()
+                parentResult.value.get()
                     .then(parentRequestResult => {
-                        let parent = parentResult;
+                        let parent = parentResult.value;
                         if (parentResult instanceof Web) {
-                            parent = new Folder(parentResult.toUrl(), (fileFolderConfig as IFile).Src ? "" : `GetFolderByServerRelativeUrl('${fileFolderConfig.Name}')`);
-                            resolve(parent as Folder);
+                            parent = new Folder(parentResult.value.toUrl(), (fileFolderConfig as IFile).Src ? "" : `GetFolderByServerRelativeUrl('${fileFolderConfig.Name}')`);
+                             Util.Resolve<Folder>(resolve, fileFolderConfig.Name, `'${fileFolderConfig.Name}' is RootFolder`, parent as Folder);
                         } else {
                             if (parentResult instanceof List) {
                                 parent = new Folder(parentRequestResult.RootFolder.__deferred.uri);
                             }
 
-                            let processing: Promise<File | Folder>;
+                            let processing: Promise<IPromiseResult<File | Folder>>;
                             if ((fileFolderConfig as IFile).Src) {
                                 processing = this.processingFileConfig(fileFolderConfig as IFile, (parent as Folder).files);
                             } else {
@@ -40,14 +42,14 @@ export class FileHandler implements ISPObjectHandler {
 
                     })
                     .catch(error => {
-                        Reject(reject, `Error while requesting parent ('${parentResult.toUrl()}') for element with the title '${fileFolderConfig.Name}': ` + error, fileFolderConfig.Name);
+                        Util.Reject<void>(reject, fileFolderConfig.Name, `Error while requesting parent ('${parentResult.value.toUrl()}') for element: '${fileFolderConfig.Name}': ` + error);
                     });
             });
         });
     }
 
-    private processingFolderConfig(folderConfig: IFolder, parentFolder: Folders): Promise<Folder> {
-        return new Promise<File | Folder>((resolve, reject) => {
+    private processingFolderConfig(folderConfig: IFolder, parentFolder: Folders): Promise<IPromiseResult<Folder | void>> {
+        return new Promise<IPromiseResult<Folder | void>>((resolve, reject) => {
             let processingText = folderConfig.ControlOption === ControlOption.Add || folderConfig.ControlOption === undefined || folderConfig.ControlOption === ""
                 ? "Add" : folderConfig.ControlOption;
             Logger.write(`Processing ${processingText} folder: '${folderConfig.Name}' to ${parentFolder.toUrl()}`, Logger.LogLevel.Info);
@@ -55,30 +57,23 @@ export class FileHandler implements ISPObjectHandler {
             let folder = parentFolder.getByName(folderConfig.Name);
             folder.get()
                 .then(folderRequestResult => {
-                    let processingPromise: Promise<Folder> = undefined;
                     switch (folderConfig.ControlOption) {
                         case ControlOption.Delete:
-                            processingPromise = this.deleteFolder(folderConfig, folder);
+                            this.deleteFolder(folderConfig, folder)
+                                .then((folderProcessingResult) => { resolve(folderProcessingResult); })
+                                .catch((error) => { reject(error); });
                             break;
                         case ControlOption.Update:
                         default: // tslint:disable-line
-                            Resolve(resolve, `Folder with the name '${folderConfig.Name}' already exists in '${parentFolder.toUrl()}'`, folderConfig.Name, folder);
+                            Util.Resolve<Folder>(resolve, folderConfig.Name, `Folder with the name '${folderConfig.Name}' already exists in '${parentFolder.toUrl()}'`, folder);
                             break;
-                    }
-
-                    if (processingPromise) {
-                        processingPromise
-                            .then((folderProcessingResult) => { resolve(folderProcessingResult); })
-                            .catch((error) => { reject(error); });
-                    } else {
-                        Logger.write("File handler processing promise is undefined!");
                     }
                 })
                 .catch((error) => {
                     if (error === "Error making GET request: Not Found") {
                         switch (folderConfig.ControlOption) {
                             case ControlOption.Delete:
-                                Reject(reject, `Folder with Name '${folderConfig.Name}' does not exists in '${folder.parentFolder}'`, folderConfig.Name);
+                                Util.Reject<void>(reject, folderConfig.Name, `Folder with Name '${folderConfig.Name}' does not exists in '${folder.parentFolder}'`);
                                 break;
                             case ControlOption.Update:
                             default: // tslint:disable-line
@@ -88,14 +83,14 @@ export class FileHandler implements ISPObjectHandler {
                                 break;
                         }
                     } else {
-                        Reject(reject, `Error while requesting folder with the title '${folderConfig.Name}' from parent '${folder.parentFolder}': ` + error, folderConfig.Name);
+                        Util.Reject<void>(reject, folderConfig.Name, `Error while requesting folder with the title '${folderConfig.Name}' from parent '${folder.parentFolder}': ` + error);
                     }
                 });
         });
     }
 
-    private processingFileConfig(fileConfig: IFile, parentFolder: Files): Promise<File> {
-        return new Promise<File | Folder>((resolve, reject) => {
+    private processingFileConfig(fileConfig: IFile, parentFolder: Files): Promise<IPromiseResult<File>> {
+        return new Promise<IPromiseResult<File>>((resolve, reject) => {
             let processingText = fileConfig.ControlOption === ControlOption.Add || fileConfig.ControlOption === undefined || fileConfig.ControlOption === ""
                 ? "Add" : fileConfig.ControlOption;
             Logger.write(`Processing ${processingText} file: '${fileConfig.Name}' in ${parentFolder.toUrl()}`, Logger.LogLevel.Info);
@@ -103,7 +98,7 @@ export class FileHandler implements ISPObjectHandler {
             let file = parentFolder.getByName(fileConfig.Name);
             file.get()
                 .then(folderRequestResult => {
-                    let processingPromise: Promise<File> = undefined;
+                    let processingPromise: Promise<IPromiseResult<File>> = undefined;
                     switch (fileConfig.ControlOption) {
                         case ControlOption.Delete:
                             processingPromise = this.deleteFile(fileConfig, file);
@@ -112,7 +107,7 @@ export class FileHandler implements ISPObjectHandler {
 
                             break;
                         default:
-                            Resolve(resolve, `File with the name '${fileConfig.Name}' already exists in '${parentFolder.toUrl()}'`, fileConfig.Name, file);
+                            Util.Resolve<File>(resolve, fileConfig.Name, `File with the name '${fileConfig.Name}' already exists in '${parentFolder.toUrl()}'`, file);
                             break;
                     }
 
@@ -128,7 +123,7 @@ export class FileHandler implements ISPObjectHandler {
                     if (error === "Error making GET request: Not Found") {
                         switch (fileConfig.ControlOption) {
                             case ControlOption.Delete:
-                                Reject(reject, `File with Name '${fileConfig.Name}' does not exists in '${parentFolder.toUrl()}'`, fileConfig.Name);
+                                Util.Reject<void>(reject, fileConfig.Name, `File with Name '${fileConfig.Name}' does not exists in '${parentFolder.toUrl()}'`);
                                 break;
                             case ControlOption.Update:
                             default: // tslint:disable-line
@@ -141,51 +136,51 @@ export class FileHandler implements ISPObjectHandler {
                                 break;
                         }
                     } else {
-                        Reject(reject, `Error while requesting file with the title '${fileConfig.Name}' from parent '${parentFolder.toUrl()}': ` + error, fileConfig.Name);
+                        Util.Reject<void>(reject, fileConfig.Name, `Error while requesting file with the title '${fileConfig.Name}' from parent '${parentFolder.toUrl()}': ` + error);
                     }
                 });
         });
     }
 
-    private addFolder(folderConfig: IFolder, parentFolder: Folders): Promise<Folder> {
-        return new Promise<Folder>((resolve, reject) => {
+    private addFolder(folderConfig: IFolder, parentFolder: Folders): Promise<IPromiseResult<Folder>> {
+        return new Promise<IPromiseResult<Folder>>((resolve, reject) => {
             parentFolder.add(folderConfig.Name)
-                .then((folderAddResult) => { Resolve(resolve, `Added item: '${folderConfig.Name}'`, folderConfig.Name, folderAddResult.folder); })
-                .catch((error) => { Reject(reject, `Error while adding folder with name '${folderConfig.Name}': ` + error, folderConfig.Name); });
+                .then((folderAddResult) => { Util.Resolve<Folder>(resolve, folderConfig.Name, `Added item: '${folderConfig.Name}'`, folderAddResult.folder); })
+                .catch((error) => { Util.Reject<void>(reject, folderConfig.Name, `Error while adding folder with name '${folderConfig.Name}': ` + error); });
         });
     }
 
-    private deleteFolder(folderConfig: IFolder, folder: Folder): Promise<Folder> {
-        return new Promise<Folder>((resolve, reject) => {
+    private deleteFolder(folderConfig: IFolder, folder: Folder): Promise<IPromiseResult<void>> {
+        return new Promise<IPromiseResult<void>>((resolve, reject) => {
             folder.delete()
-                .then(() => { Resolve(resolve, `Deleted folder: '${folderConfig.Name}'`, folderConfig.Name); })
-                .catch((error) => { Reject(reject, `Error while deleting folder with name '${folderConfig.Name}': ` + error, folderConfig.Name); });
+                .then(() => { Util.Resolve<void>(resolve, folderConfig.Name, `Deleted folder: '${folderConfig.Name}'`); })
+                .catch((error) => { Util.Reject<void>(reject, `Error while deleting folder with name '${folderConfig.Name}': ` + error, folderConfig.Name); });
         });
     }
 
-    private addFile(fileConfig: IFile, parentFolder: Files): Promise<File> {
-        return new Promise<File>((resolve, reject) => {
+    private addFile(fileConfig: IFile, parentFolder: Files): Promise<IPromiseResult<File>> {
+        return new Promise<IPromiseResult<File>>((resolve, reject) => {
             let file = fs.readFileSync(fileConfig.Src);
             parentFolder.add(fileConfig.Name, file, mime.lookup(fileConfig.Name))
-                .then((fileAddResult) => { Resolve(resolve, `Added file: '${fileConfig.Name}'`, fileConfig.Name, fileAddResult.file); })
-                .catch((error) => { Reject(reject, `Error while adding folder with name '${fileConfig.Name}': ` + error, fileConfig.Name); });
+                .then((fileAddResult) => { Util.Resolve<File>(resolve, fileConfig.Name, `Added file: '${fileConfig.Name}'`, fileAddResult.file); })
+                .catch((error) => { Util.Reject<void>(reject, fileConfig.Name, `Error while adding folder with name '${fileConfig.Name}': ` + error); });
         });
     }
 
-    private deleteFile(fileConfig: IFile, file: File): Promise<File> {
-        return new Promise<File>((resolve, reject) => {
+    private deleteFile(fileConfig: IFile, file: File): Promise<IPromiseResult<File>> {
+        return new Promise<IPromiseResult<File>>((resolve, reject) => {
             file.delete()
-                .then(() => { Resolve(resolve, `Deleted file: '${fileConfig.Name}'`, fileConfig.Name); })
-                .catch((error) => { Reject(reject, `Error while deleting file with name '${fileConfig.Name}': ` + error, fileConfig.Name); });
+                .then(() => { Util.Resolve(resolve, fileConfig.Name, `Deleted file: '${fileConfig.Name}'`); })
+                .catch((error) => { Util.Reject(reject, fileConfig.Name, `Error while deleting file with name '${fileConfig.Name}': ` + error); });
         });
     }
 
-    private updateFile(fileConfig: IFile, file: File): Promise<File> {
-        return new Promise<File>((resolve, reject) => {
+    private updateFile(fileConfig: IFile, file: File): Promise<IPromiseResult<Item>> {
+        return new Promise<IPromiseResult<Item>>((resolve, reject) => {
             let properties = this.createProperties(fileConfig.Properties as IItem);
             file.listItemAllFields.update(properties)
-                .then((itemUpdateResult) => { Resolve(resolve, `Updated item: '${fileConfig.Name}'`, fileConfig.Name, itemUpdateResult.item); })
-                .catch((error) => { Reject(reject, `Error while updating item with title '${fileConfig.Name}': ` + error, fileConfig.Name); });
+                .then((itemUpdateResult) => { Util.Resolve<Item>(resolve, fileConfig.Name, `Updated item: '${fileConfig.Name}'`, itemUpdateResult.item); })
+                .catch((error) => { Util.Reject(reject, `Error while updating item with title '${fileConfig.Name}': ` + error, fileConfig.Name); });
         });
     }
 
