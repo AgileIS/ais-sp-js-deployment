b504b1fa65e009ed0c708d0d22365ee5e45f15e1
Yücel Tasyol <ytasyol@agile-is.de>
2016-10-13 14:19:53 +0200

    Deleted /src/Constants

--- src/Constants/AuthenticationType.ts
+++
@@ -1,4 +0,0 @@
-export class AuthenticationType {
-    public static BASIC = "Basic";
-    public static NTLM = "Ntlm";
-}
--- src/Constants/ControlOption.ts
+++
@@ -1,5 +0,0 @@
-export class ControlOption {
-    public static ADD = "Add";
-    public static UPDATE = "Update";
-    public static DELETE = "Delete";
-}
