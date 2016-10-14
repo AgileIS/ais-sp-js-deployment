989397daffcb0a0a5fedf35cacf7fdb78e7fa66c
Yücel Tasyol <ytasyol@agile-is.de>
2016-10-14 10:46:29 +0200

    fixed date in loggers

--- src/logger/fileLogger.ts
+++ src/logger/fileLogger.ts
@@ -9,14 +9,14 @@ export class FileLogger implements LogListener {
         this.logPrefix = logPrefix ? logPrefix + "# " : "";
         if (!FileLogger.file) {
             let now = new Date();
-            FileLogger.file = `AISDEPLOY-${now.getFullYear()}${now.getMonth()}${now.getDay()}-${now.getHours()}${now.getMinutes()}.log`;
+            FileLogger.file = `AISDEPLOY-${now.getFullYear()}${now.getMonth() + 1}${now.getDate()}-${now.getHours()}${now.getMinutes()}.log`;
         }
     }
 
     public log(entry: LogEntry) {
         let now = new Date();
         let log = `[${Logger.LogLevel[entry.level]}] ${this.logPrefix}` +
-            `${now.getDay()}.${now.getMonth()}.${now.getFullYear()}  ${now.getHours()}:${now.getMinutes()}:${now.getMilliseconds()}: ${entry.message}`;
+            `${now.getDate()}.${now.getMonth() + 1}.${now.getFullYear()}  ${now.getHours()}:${now.getMinutes()}:${now.getMilliseconds()}: ${entry.message}`;
         if (entry.data) {
             log += `- ${entry.data}`;
         }
